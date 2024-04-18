e91afdc14dc3992cea5f78ca25d64fd94bf31797
Tommi Hovi <tommi.hovi@gmail.com>
2024-04-18 23:58:44 +0300

    Rename utg and btn to row and column players
    - Some small changes to text

--- main.py
+++ main.py
@@ -10,102 +10,103 @@ from graphviz import Digraph
 RANGE_INCREMENTS = 5
 
 
-# prob for random number in [u, 1] being bigger than in [b, 1]
-def u_win_prob(u, b):
-    if u > 1 or u < 0 or b > 1 or b < 0:
+# probability for random number in [r, 1] of being bigger than random number in [c, 1]
+def r_win_prob(r, c):
+    if r > 1 or r < 0 or c > 1 or c < 0:
         raise ValueError("u and b should be in [0, 1]")
-    if u >= b:
+    if r >= c:
         # The math is based on looking the prob mass function
         # of the combined random variables, so basically 2d geometry.
-        triangle_area = 0.5 * (1 - u) * (1 - u)
-        rectangle_area = (u - b) * (1 - u)
+        triangle_area = 0.5 * (1 - r) * (1 - r)
+        rectangle_area = (r - c) * (1 - r)
         a_win_area = triangle_area + rectangle_area
-        total_area = (1 - u) * (1 - b)
+        total_area = (1 - r) * (1 - c)
 
         return a_win_area / total_area
-    # b > u
-    return 1 - u_win_prob(b, u)
+    # r < c
+    return 1 - r_win_prob(c, r)
 
 
-def u_bet_prob(u, b):
-    return 1 - u
+def r_bet_prob(r, c):
+    return 1 - r
 
 
-def u_fold_prob(u, b):
-    return u
+def r_fold_prob(r, c):
+    return r
 
 
-def b_call_prob(u, b):
-    return 1 - b
+def c_call_prob(r, c):
+    return 1 - c
 
 
-def b_fold_prob(u, b):
-    return b
+def c_fold_prob(r, c):
+    return c
 
 
-def u_bet_b_fold_prob(u, b):
-    u_bet = u_bet_prob(u, b)
-    b_fold = b_fold_prob(u, b)
+def r_bet_c_fold_prob(r, c):
+    r_bet = r_bet_prob(r, c)
+    c_fold = c_fold_prob(r, c)
 
-    return u_bet * b_fold
+    return r_bet * c_fold
 
 
-def u_bet_b_call_u_win_prob(u, b):
-    u_bet = u_bet_prob(u, b)
-    b_call = b_call_prob(u, b)
-    a_win = u_win_prob(u, b)
+def r_bet_c_call_r_win_prob(r, c):
+    r_bet = r_bet_prob(r, c)
+    c_call = c_call_prob(r, c)
+    r_win = r_win_prob(r, c)
 
-    return u_bet * b_call * a_win
+    return r_bet * c_call * r_win
 
 
 def u_bet_b_call_b_win_prob(u, b):
-    u_bet = u_bet_prob(u, b)
-    b_call = b_call_prob(u, b)
-    a_win = u_win_prob(u, b)
+    u_bet = r_bet_prob(u, b)
+    b_call = c_call_prob(u, b)
+    a_win = r_win_prob(u, b)
 
     return u_bet * b_call * (1 - a_win)
 
 
-def btn_ev(u, b, pot_size, bet_size):
+def col_ev(r, c, pot_size, bet_size):
     total_ev = 0
 
-    # U fold
-    total_ev += u_fold_prob(u, b) * pot_size
-    # U bet, B fold
-    total_ev += u_bet_b_fold_prob(u, b) * 0
-    # U bet, B call, U win
-    total_ev += u_bet_b_call_u_win_prob(u, b) * -bet_size
-    # U bet, B call, B win
-    total_ev += u_bet_b_call_b_win_prob(u, b) * (pot_size + bet_size)
+    # R fold
+    total_ev += r_fold_prob(r, c) * pot_size
+    # R bet, C fold
+    total_ev += r_bet_c_fold_prob(r, c) * 0
+    # R bet, C call, R win
+    total_ev += r_bet_c_call_r_win_prob(r, c) * -bet_size
+    # R bet, C call, C win
+    total_ev += u_bet_b_call_b_win_prob(r, c) * (pot_size + bet_size)
 
     return total_ev
 
 
-def utg_ev(u, b, pot_size, bet_size):
+def row_ev(r, c, pot_size, bet_size):
     total_ev = 0
 
-    # U fold
-    total_ev += u_fold_prob(u, b) * 0
-    # U bet, B fold
-    total_ev += u_bet_b_fold_prob(u, b) * pot_size
-    # U bet, B call, U win
-    total_ev += u_bet_b_call_u_win_prob(u, b) * (pot_size + bet_size)
-    # U bet, B call, B win
-    total_ev += u_bet_b_call_b_win_prob(u, b) * -bet_size
+    # R fold
+    total_ev += r_fold_prob(r, c) * 0
+    # R bet, C fold
+    total_ev += r_bet_c_fold_prob(r, c) * pot_size
+    # R bet, C call, R win
+    total_ev += r_bet_c_call_r_win_prob(r, c) * (pot_size + bet_size)
+    # R bet, C call, C win
+    total_ev += u_bet_b_call_b_win_prob(r, c) * -bet_size
 
     return total_ev
 
 
-def get_possible_ne_points(btn_evs):
-    length = len(btn_evs)
+def get_possible_ne_points(col_evs):
+    length = len(col_evs)
 
-    # BTN possible Nash Equilibrium points are the maximum column values in each row
-    btn_possible_ne_points = set()
+    # col possible Nash Equilibrium points are the maximum column values in each row
+    # These are the best responses for column player against each row player's strategy
+    col_possible_ne_points = set()
     for row_number in range(length):
         max_value = float("-inf")
         max_indexes = []
         for col_number in range(length):
-            curr_value = btn_evs[row_number][col_number]
+            curr_value = col_evs[row_number][col_number]
 
             if abs(curr_value - max_value) < 1e-6:
                 max_indexes.append(col_number)
@@ -114,15 +115,16 @@ def get_possible_ne_points(btn_evs):
                 max_indexes = [col_number]
 
         for col_number in max_indexes:
-            btn_possible_ne_points.add((row_number, col_number))
+            col_possible_ne_points.add((row_number, col_number))
 
-    # UTG possible Nash Equilibrium points are the minimum row values in each column
-    utg_possible_ne_points = set()
+    # row possible Nash Equilibrium points are the minimum row values in each column
+    # These are the best responses for row player against each column player's strategy
+    row_possible_ne_points = set()
     for col_number in range(length):
         min_value = float("inf")
         min_indexes = []
         for row_number in range(length):
-            curr_value = btn_evs[row_number][col_number]
+            curr_value = col_evs[row_number][col_number]
 
             if abs(curr_value - min_value) < 1e-6:
                 min_indexes.append(row_number)
@@ -131,10 +133,10 @@ def get_possible_ne_points(btn_evs):
                 min_indexes = [row_number]
 
         for row_number in min_indexes:
-            utg_possible_ne_points.add((row_number, col_number))
+            row_possible_ne_points.add((row_number, col_number))
 
 
-    return btn_possible_ne_points, utg_possible_ne_points
+    return col_possible_ne_points, row_possible_ne_points
 
 
 def get_range():
@@ -142,17 +144,17 @@ def get_range():
 
 
 def create_heatmap(pot_size=1, bet_size=1):
-    btn_evs = []
-    for u_i in get_range():
+    col_evs = []
+    for r_i in get_range():
         row_list = []
-        u = u_i / 100
+        r = r_i / 100
 
-        for b_i in get_range():
-            b = b_i / 100
-            row_list.append(btn_ev(u, b, pot_size, bet_size))
-        btn_evs.append(row_list)
+        for c_i in get_range():
+            c = c_i / 100
+            row_list.append(col_ev(r, c, pot_size, bet_size))
+        col_evs.append(row_list)
 
-    btn_nes, utg_nes = get_possible_ne_points(btn_evs)
+    col_nes, row_nes = get_possible_ne_points(col_evs)
 
     axis_labels = [str(i / 100) for i in get_range()]
 
@@ -161,9 +163,7 @@ def create_heatmap(pot_size=1, bet_size=1):
     fig, ax = plt.subplots(figsize=(20,10))
 
     sns.heatmap(
-        data=btn_evs,
-        # vmin=0,
-        # vmax=pot_size,
+        data=col_evs,
         xticklabels=axis_labels,
         yticklabels=axis_labels,
         annot=True,
@@ -174,32 +174,28 @@ def create_heatmap(pot_size=1, bet_size=1):
 
     ax.invert_yaxis()
     ax.set_yticklabels(ax.get_yticklabels(), rotation=90)
-    ax.set(xlabel="BTN minimum hand strength", ylabel="UTG minimum hand strength")
+    ax.set(xlabel="Column player minimum hand strength", ylabel="Row player minimum hand strength")
     cbar = ax.collections[0].colorbar
-    cbar.set_label("BTN EV")
+    cbar.set_label("Col EV")
 
-    # Add row max-values
-    for btn_ne in btn_nes:
-        row, col = btn_ne
+    # Add row max-values. These are the best responses for the column player for the given row player strategy
+    for col_ne in col_nes:
+        row, col = col_ne
         circles = patches.Circle(
             (col + 0.5, row + 0.5), 0.5, edgecolor="black", facecolor="red"
         )
         ax.add_patch(circles)
 
-    # Add col min values
-    for utg_ne in utg_nes:
-        row, col = utg_ne
+    # Add col min values. These are the best responses for the row player for the given column player strategy
+    for row_ne in row_nes:
+        row, col = row_ne
         circles = patches.Circle(
             (col + 0.5, row + 0.5), 0.5, edgecolor="black", facecolor="blue"
         )
         ax.add_patch(circles)
 
-    # Draw a circle around the Nash Equilibrium point. The point is hardcoded,
-    # but could be calculated from the data, since this is the point where the
-    # value is the maximum in the row and minimum in the column.
-
-    # Actual NE points
-    ne_points = btn_nes.intersection(utg_nes)
+    # Actual NE points. These are the points where the row player's strategy is the best response to the column player's strategy
+    ne_points = col_nes.intersection(row_nes)
 
     for ne_point in ne_points:
         row, col = ne_point
@@ -217,7 +213,7 @@ def create_heatmap(pot_size=1, bet_size=1):
         bbox_to_anchor=(1.15, 1),
         loc="upper left",
     )
-    plt.title(f"BTN EV for Pot size {pot_size}, Bet size {bet_size}")
+    plt.title(f"Col EV for Pot size {pot_size}, Bet size {bet_size}")
 
     return fig
 
@@ -226,21 +222,21 @@ def graphviz_chart():
     dot = Digraph(graph_attr={'rankdir':'LR'})
 
     # Add nodes
-    dot.node('ua1', 'UTG choice')
-    dot.node('bw1', 'BTN wins', shape='box', style='filled', fillcolor='orange')
-    dot.node('uw1', 'UTG wins', shape='box', style='filled', fillcolor='lightblue')
-    dot.node('ba1', 'BTN choice')
+    dot.node('rpc', "Row player's choice")
+    dot.node('cpw1', 'Column player wins', shape='box', style='filled', fillcolor='orange')
+    dot.node('rpw1', 'Row player wins', shape='box', style='filled', fillcolor='lightblue')
+    dot.node('cpc', "Column player's choice")
     dot.node('sd', 'Showdown')
-    dot.node('bw2', 'BTN wins', shape='box', style='filled', fillcolor='orange')
-    dot.node('uw2', 'UTG wins', shape='box', style='filled', fillcolor='lightblue')
+    dot.node('cpw2', 'Column player wins', shape='box', style='filled', fillcolor='orange')
+    dot.node('rpw2', 'Row player wins', shape='box', style='filled', fillcolor='lightblue')
 
     # Add edges
-    dot.edge('ua1', 'bw1', label='Fold')
-    dot.edge('ua1', 'ba1', label='Bet')
-    dot.edge('ba1', 'uw1', label='Fold')
-    dot.edge('ba1', 'sd', label='Call')
-    dot.edge('sd', 'bw2', label='BTN has better hand')
-    dot.edge('sd', 'uw2', label='UTG has better hand')
+    dot.edge('rpc', 'cpw1', label='Fold')
+    dot.edge('rpc', 'cpc', label='Bet')
+    dot.edge('cpc', 'rpw1', label='Fold')
+    dot.edge('cpc', 'sd', label='Call')
+    dot.edge('sd', 'cpw2', label='Column player has better hand')
+    dot.edge('sd', 'rpw2', label='Row player has better hand')
 
     return dot
 
@@ -256,15 +252,15 @@ def run_st():
 
                 ## Simplified poker game
 
-                This game has 2 players: UTG (short for under-the-gun) and BTN (short for button). These terms are poker jargon and refer to the positions of the players at the table.
+                This game has 2 players: Row player and Column player.
 
-                Players hands are represented as a number between 0 and 1. If there is a showdon, the player with the higher number wins the hand. For simplicity we assume the numbers cannot be equal, so no ties.
+                To make the calculations simpler, we will represent player's poker hand as a number between 0 and 1. The player with the higher number wins, if there is a showdown. We will assume there is no ties.
 
                 Initially the pot has X chips in it.
 
-                UTG is the first to act and they can either fold or bet Z chips. If UTG folds, BTN wins the pot. If UTG bets, BTN can either fold or call and pay Z chips. If BTN folds, UTG wins the pot. If BTN calls, the players hands are compared and the player with the higher hand wins the pot and the chips that were bet.
+                Row player is the first to act and they can either fold or bet Z chips. If Row player folds, Column player wins the pot. If Row player bets, Column player can either fold or call and pay Z chips. If Column player folds, Row player wins the pot. If Column player calls, the players hands are compared and the player with the higher hand wins the pot and the chips that were bet.
 
-                The following chart shows the possible outcomes:
+                The following chart shows the possible outcomes and order of choices:
                 """)
     gc = graphviz_chart()
     st.graphviz_chart(gc)
@@ -274,11 +270,11 @@ def run_st():
 
                 Both players want to maximize their expected winnings. The only thing they choose is what is the minimum hand strength they are willing to play with. For example if a player has strategy of playing every hand with strength 0.5 or higher, they will fold every hand with strength lower than 0.5.
 
-                We will calculate the expected value for BTN player for each possible strategy pair and plot these in a heatmap below. So BTN player wants to maximize this value and UTG minimize it, because the game is zero-sum game (technically there is some extra chips in the pot). You can change the Pot size and Bet size values to adjust calculations.
+                We will calculate the EV for Column player for each possible strategy pair and plot these in a heatmap below. So Column player wants to maximize this. Because the game is zero-sum game (technically there is some extra chips in the pot), it means the Row player's EV is (X - Column player EV), where X is the initial pot size. For Row player to maximize their EV, it is equivalent for them to minimize Column player's EV.
 
-                Because the heatmap only shows strategies in 0.05 increments, it might not always show the Nash Equilibrium point with certain bet size and pot size combinations. However the nash equilibrium still always exists.
+                The X axis is the minimum hand strength of the Column player and the Y axis is the minimum hand strength of the Row player. The red circles in the heatmap show the maximum value for each row. This is the best strategy for the Column player If Row is player the strategy indicated by that row. Similarly the blue circle in the heatmap shows the minimum value for each column. This is the best strategy for the Row player if Column player is playing the strategy indicated by that column. The purple circle shows the Nash Equilibrium point, where that cell is both the maximum value in the row and minimum value in the column, so neither player has anything to gain by changing their strategy if the other player doesn't change theirs.
 
-                The X axis is the minimum hand strength of the BTN player and the Y axis is the minimum hand strength of the UTG player. The red circles in the heatmap show the maximum value for each row. This is the best strategy for the BTN player IF UTG is player the strategy indicated by that row. Similarly the blue circle in the heatmap shows the minimum value for each column. This is the best strategy for the UTG player IF BTN is playing the strategy indicated by that column. The purple circle shows the Nash Equilibrium point, where that cell is both the maximum value in the row and minimum value in the column, so neither player has anything to gain by changing their strategy if the other player doesn't change theirs.
+                You can change the Pot size and Bet size values to adjust calculations. Because the heatmap only shows strategies in 0.05 increments, it might not always show the Nash Equilibrium point with certain bet size and pot size combinations. However the nash equilibrium still always exists.
                 """)
 
     col1, col2 = st.columns(2)
